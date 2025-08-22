db74fca3fbcac8b6497fc548d739c6d9e77652c4
Ubuntu <ubuntu@ip-172-31-20-39.eu-north-1.compute.internal>
2025-08-22 12:09:00 +0000

    Added start script that extracts variables from aws secrets manager

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+SECRET_MANAGER_SECRET_ID=$(cat .env | grep SECRET_MANAGER_SECRET_ID | cut -d '=' -f 2)
+
+echo "Fetching secrets from AWS Secrets Manager..."
+
+SECRETS_JSON=$(aws secretsmanager get-secret-value --secret-id "$SECRET_MANAGER_SECRET_ID" --query SecretString --output text)
+
+if [ -z "$SECRETS_JSON" ]; then
+    echo "Error: Could not retrieve secret from AWS Secrets Manager."
+    exit 1
+fi
+
+export MYSQL_USER=$(echo "$SECRETS_JSON" | jq -r '.MYSQL_USER')
+export MYSQL_PASSWORD=$(echo "$SECRETS_JSON" | jq -r '.MYSQL_PASSWORD')
+export MYSQL_DATABASE=$(echo "$SECRETS_JSON" | jq -r '.MYSQL_ROOT_PASSWORD')
+export MYSQL_DATABASE=$(echo "$SECRETS_JSON" | jq -r '.MYSQL_DATABASE')
+
+
+
+echo "Secrets exported. Starting Docker Compose..."
+
+docker-compose down
+docker system prune -a -f --filter "label!=keep=true"
+docker-compose up -d
+
+#Remove Secrets from Environment
+unset MYSQL_USER
+unset MYSQL_PASSWORD
+unset MYSQL_DATABASE
+unset MYSQL_ROOT_PASSWORD
+
+
